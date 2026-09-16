251bb3b285699dd770742e57c42811a4ee392b79
Rawat1810 <159500355+Rawat1810@users.noreply.github.com>
2026-09-16 14:45:17 +0530

    Update ProjectTesting.java

--- ProjectTesting.java
+++ ProjectTesting.java
@@ -1,5 +1,5 @@
 public class ProjectTesting {
     public static void main(String[] args) {
-        System.out.println("Building Project");
+        System.out.println("Testing Project");
     }
 }
